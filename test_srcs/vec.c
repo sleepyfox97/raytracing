1601222e781c0abf796b33c363fe83bc99ce75f1
sleepyfox97 <5851kanda@gmail.com>
2021-04-12 06:00:52 +0200

    fix vec.c

--- test_srcs/vec.c
+++ test_srcs/vec.c
@@ -58,6 +58,13 @@ t_vec3 ft_make_nomalvec(t_vec3 v)
 }
 
 
+t_vec3 ft_set_vecele(t_vec3 v, double x, double y, double z)
+{
+    v.x = x;
+    v.y = y;
+    v.z = z;
+    return (v);
+}
 
 // int main()
 // {
